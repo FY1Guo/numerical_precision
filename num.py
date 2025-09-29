5eeb9838f6a79ed8c37a3f39fb4e462711b03a9e
Carter <cmatties2003@gmail.com>
2025-09-28 21:30:20 -0400

    Added division method

--- num.py
+++ num.py
@@ -51,6 +51,49 @@ def mult(a1, b1, a2, b2):
     return(a, b)
     
 #division
-
+def div(a1, b1, a2, b2):
+    '''
+    For two numbers (a1,b1) and (a2,b2), returns the quotient (a1/a2, b1-b2)
+    '''
+    if a2==0:
+        raise ZeroDivisionError
+    if a1==0:
+        return (0,0)
+    
+    sign_1 = 1-2*(a1<0)
+    sign_2 = 1-2*(a2<0)
+    a1 *= sign_1
+    a2 *= sign_2
+    
+    num_digits_1 = len(str(a1))
+    num_digits_2 = len(str(a2))
+    exponent = max(num_digits_2 - num_digits_1, 0) + 20
+    
+    quotient = long_division(a1*10**(exponent), a2)
+    
+    return (quotient*sign_1*sign_2, b1-b2-exponent)
+    
+def long_division(dividend, divisor):
+    '''
+    Compute dividend/divisor using long division, discarding the remainder
+    '''
+    dividend_string = str(dividend)
+    remainder = 0
+    quotient = ""
+    num_dividend_digits = 1
+    
+    for i in range(len(dividend_string)):
+        quotient_digit = 0
+        dividend_chunk = dividend_string[:num_dividend_digits]
+        if divisor <= int(dividend_chunk):
+            while (quotient_digit + 1) * divisor <= int(dividend_chunk):
+                quotient_digit += 1
+            remainder = int(dividend_chunk) - (quotient_digit * divisor)
+            dividend_string = str(remainder) + dividend_string[num_dividend_digits:]
+            num_dividend_digits = len(str(remainder))+1
+        else:
+            num_dividend_digits += 1
+        quotient += str(quotient_digit)
+    return int(quotient)
 #test
 
