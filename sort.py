622cbe337d9f0a7ab248474ec73917e2f64bded2
Carter <cmatties2003@gmail.com>
2025-10-07 13:40:08 -0400

    Change sorting benchmarks and add more plots

--- sort.py
+++ sort.py
@@ -44,19 +44,55 @@ def merge_sort(arr):
     sort(0, len(arr)) #exlusive of end point
     return arr
 
-N_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200,300,400, 500, 750, 1000, 1500, 2000, 2500, 5000]
+N_list_bubble = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200,300,400, 500]
+N_list_merge = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200,300,400, 500, 750, 1000, 1500, 2000, 2500, 5000, 6000, 7000, 8000, 9000, 10000]
 bubble_times = []
 merge_times = []
 
-for N in N_list:
-    a_list = rand.randint(int(-10), int(10), size = N)
-    b_list = rand.randint(int(-5), int(5), size = N)
-    number_list = [HPNumber(a_list[i], b_list[i]) for i in range(N)]
+for N in N_list_bubble:
+    total_time_bubble = 0
     print(N)
-    bubble_times.append(timeit.timeit("bubble_sort(number_list)", globals = globals(), number = 5))
-    merge_times.append(timeit.timeit("merge_sort(number_list)", globals = globals(),number = 5))
+    for i in range(5):
+        a_list = rand.randint(int(-10), int(10), size = N)
+        b_list = rand.randint(int(-2), int(2), size = N)
+        number_list = [HPNumber(a_list[i], b_list[i]) for i in range(N)]
+        total_time_bubble += timeit.timeit("bubble_sort(number_list)", globals = globals(), number = 1)
+    bubble_times.append(total_time_bubble/5)
     
-plt.plot(N_list, bubble_times, label = "Bubble sort")
-plt.plot(N_list, merge_times, label = "Merge sort")
-plt.legend()
+    
+for N in N_list_merge:
+    total_time_merge = 0
+    print(N)
+    for i in range(5):
+        a_list = rand.randint(int(-10), int(10), size = N)
+        b_list = rand.randint(int(-2), int(2), size = N)
+        number_list = [HPNumber(a_list[i], b_list[i]) for i in range(N)]
+        total_time_merge += timeit.timeit("merge_sort(number_list)", globals = globals(),number = 1)
+    merge_times.append(total_time_merge/5)
+
+plt.title("Bubble sort")
+plt.plot(N_list_bubble, bubble_times)
+plt.xlabel("List length")
+plt.ylabel("Time")
+plt.show()
+
+plt.clf()
+plt.title("Merge sort")
+plt.plot(N_list_merge, merge_times)
+plt.xlabel("List length")
+plt.ylabel("Time")
+plt.show()
+
+plt.clf()
+plt.title(r"Comparing Bubble sort to $N^2$")
+plt.plot(N_list_bubble, np.array(bubble_times)/np.array(N_list_bubble)**2)
+plt.xlabel(r"$N$")
+plt.ylabel(r"Time/$N^2$")
+plt.show()
+
+plt.clf()
+plt.title(r"Comparing merge sort to $N\ln N$")
+plt.xlabel(r"$N$")
+plt.ylabel(r"Time/$N\ln N$")
+plt.plot(N_list_merge, np.array(merge_times)/(np.array(N_list_merge)*np.log(np.array(N_list_merge))))
 plt.show()
