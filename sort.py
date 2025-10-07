55b0ef7203754e4fd5fbeeaf32684e1aa6ccc99e
FY1Guo <fangyi.guo2@gmail.com>
2025-10-07 14:18:29 -0400

    add and refine normalization plots, savefig, black codes

--- sort.py
+++ sort.py
@@ -4,21 +4,23 @@ import numpy.random as rand
 import timeit
 import matplotlib.pyplot as plt
 
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
 
 def merge_sort(arr):
     def merge(left, mid, right):
         left_part = arr[left:mid]
         right_part = arr[mid:right]
-        i = 0 #left half index
-        j = 0 #right half index
-        k = left #write index on arr
+        i = 0  # left half index
+        j = 0  # right half index
+        k = left  # write index on arr
         while i < len(left_part) and j < len(right_part):
             if left_part[i] <= right_part[j]:
                 arr[k] = left_part[i]
@@ -35,17 +37,20 @@ def merge_sort(arr):
             arr[k] = right_part[j]
             j = j + 1
             k = k + 1
+
     def sort(left, right):
         if right - left > 1:
             mid = (left + right) // 2
             sort(left, mid)
             sort(mid, right)
             merge(left, mid, right)
-    sort(0, len(arr)) #exlusive of end point
+
+    sort(0, len(arr))  # exlusive of end point
     return arr
 
-N_list_bubble = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200,300,400, 500, 750, 1000, 1500, 2000, 2500, 5000]
-N_list_merge = np.logspace(2, 5, 20, dtype=int)
+
+N_list_bubble = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500]
+N_list_merge = np.linspace(10, 10000, 20, dtype=int)
 bubble_times = []
 merge_times = []
 
@@ -53,46 +58,59 @@ for N in N_list_bubble:
     total_time_bubble = 0
     print(N)
     for i in range(5):
-        a_list = rand.randint(int(-10), int(10), size = N)
-        b_list = rand.randint(int(-2), int(2), size = N)
+        a_list = rand.randint(int(-10), int(10), size=N)
+        b_list = rand.randint(int(-2), int(2), size=N)
         number_list = [HPNumber(a_list[i], b_list[i]) for i in range(N)]
-        total_time_bubble += timeit.timeit("bubble_sort(number_list)", globals = globals(), number = 1)
-    bubble_times.append(total_time_bubble/5)
-    
-    
+        total_time_bubble += timeit.timeit(
+            "bubble_sort(number_list)", globals=globals(), number=1
+        )
+    bubble_times.append(total_time_bubble / 5)
+
+
 for N in N_list_merge:
     total_time_merge = 0
     print(N)
     for i in range(5):
-        a_list = rand.randint(int(-10), int(10), size = N)
-        b_list = rand.randint(int(-2), int(2), size = N)
+        a_list = rand.randint(int(-10), int(10), size=N)
+        b_list = rand.randint(int(-2), int(2), size=N)
         number_list = [HPNumber(a_list[i], b_list[i]) for i in range(N)]
-        total_time_merge += timeit.timeit("merge_sort(number_list)", globals = globals(),number = 1)
-    merge_times.append(total_time_merge/5)
+        total_time_merge += timeit.timeit(
+            "merge_sort(number_list)", globals=globals(), number=1
+        )
+    merge_times.append(total_time_merge / 5)
 
+
+plt.figure()
 plt.title("Bubble sort")
 plt.plot(N_list_bubble, bubble_times)
 plt.xlabel("List length")
 plt.ylabel("Time")
+plt.savefig("Bubble-Full.png")
 plt.show()
 
-plt.clf()
+plt.figure()
 plt.title("Merge sort")
 plt.plot(N_list_merge, merge_times)
 plt.xlabel("List length")
 plt.ylabel("Time")
+plt.savefig("Merge-Full.png")
 plt.show()
 
-plt.clf()
+plt.figure()
 plt.title(r"Comparing Bubble sort to $N^2$")
-plt.plot(N_list_bubble, np.array(bubble_times)/np.array(N_list_bubble)**2)
+plt.plot(N_list_bubble, np.array(bubble_times) / np.array(N_list_bubble) ** 2)
 plt.xlabel(r"$N$")
 plt.ylabel(r"Time/$N^2$")
+plt.savefig("Bubble-Proportional.png")
 plt.show()
 
-plt.clf()
+plt.figure()
 plt.title(r"Comparing merge sort to $N\ln N$")
 plt.xlabel(r"$N$")
 plt.ylabel(r"Time/$N\ln N$")
-plt.plot(N_list_merge, np.array(merge_times)/(np.array(N_list_merge)*np.log(np.array(N_list_merge))))
+plt.plot(
+    N_list_merge,
+    np.array(merge_times) / (np.array(N_list_merge) * np.log(np.array(N_list_merge))),
+)
+plt.savefig("Merge-Proportional.png")
 plt.show()
